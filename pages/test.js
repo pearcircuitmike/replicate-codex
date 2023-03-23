eaa18edb7f6ee48a3dabcf3d1af0b43aa115f0c7
Michael Young <msyoung2012@gmail.com>
2023-03-23 19:42:41 -0400

    add supabase
    initial test connection

---
+++ pages/test.js
@@ -0,0 +1,26 @@
+import { createClient } from "@supabase/supabase-js";
+
+export default function MyPage() {
+  async function getData() {
+    // Connect to the Supabase database
+    const supabaseUrl = "https://mnhpqpybfurtxpruvltx.supabase.co";
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_CLIENT_API_KEY;
+    const supabase = createClient(supabaseUrl, supabaseKey);
+
+    // Query the testTable table for data
+    const { data, error } = await supabase.from("testTable").select("id");
+
+    // Handle any errors and log the data
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(data);
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+    </div>
+  );
+}
