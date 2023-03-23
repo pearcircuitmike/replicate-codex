b4aeb54ac341d1959a5d6bf076e736999b9a309c
Michael Young <msyoung2012@gmail.com>
2023-03-22 21:38:20 -0400

    add microsoft clarity
    for improved visibility on users

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,15 @@ export default function App({ Component, pageProps }) {
           gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
         `}
           </Script>
+          <Script id="microsoft-clarity">
+            {`
+           (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "gcvhh8mh07");
+        `}
+          </Script>
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
