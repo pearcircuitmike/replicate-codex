d0025c84ba6e825b2947737fa7f3c533f83a40d1
Michael Young <msyoung2012@gmail.com>
2023-04-01 17:16:17 -0400

    Add gallery view
    A bit jumpy but much more refined

--- pages/components/GalleryView.js
+++ pages/components/GalleryView.js
@@ -1,15 +1,6 @@
 import * as React from "react";
-import {
-  Box,
-  Checkbox,
-  Heading,
-  Image,
-  Tag,
-  SimpleGrid,
-  Text,
-} from "@chakra-ui/react";
-import Link from "next/link";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import ModelCard from "./ModelCard";
 
 export default function GalleryView({
   data,
@@ -64,89 +55,9 @@ export default function GalleryView({
                     .toLowerCase()
                     .includes(searchValue.toString().toLocaleLowerCase())))
           )
-          .map(
-            ({
-              creator,
-              modelName,
-              description,
-              tags,
-              example,
-              modelUrl,
-              runs,
-              costToRun,
-              id,
-              lastUpdated,
-            }) => (
-              <Box
-                key={id}
-                borderColor="gray.200"
-                backgroundColor="white"
-                borderWidth="1px"
-                borderRadius="2px"
-                display="flex"
-                flexDirection="column"
-                height="100%"
-                p={2}
-              >
-                <Box height="250px" m="1px">
-                  <Link href={`/models/${id}`}>
-                    <Image
-                      src={
-                        example
-                          ? example
-                          : "https://upload.wikimedia.org/wikipedia/commons/d/dc/No_Preview_image_2.png"
-                      }
-                      alt="example"
-                      width="100%"
-                      maxW="240px"
-                      maxH="240px"
-                      margin="auto"
-                      objectFit="cover"
-                      objectPosition="center"
-                    />
-                  </Link>
-                </Box>
-
-                <Heading fontSize="lg" as="h2">
-                  <a href={`/models/${id}`}>{modelName}</a>
-                </Heading>
-
-                <Text mt={1}>
-                  <Tag colorScheme={"teal"}>{tags}</Tag>
-                </Text>
-
-                <Box height="80px" mt={1}>
-                  <Text fontSize="sm" color={"gray.500"} noOfLines={3}>
-                    {description}
-                  </Text>
-                </Box>
-
-                <Text fontSize="sm">Runs: {runs.toLocaleString()}</Text>
-                <Text fontSize="sm">
-                  Cost to Run: ${costToRun ? costToRun.toLocaleString() : "-"}
-                </Text>
-                <Text fontSize="sm">
-                  Creator:{" "}
-                  <Link href={`/creators/${creator}`}>
-                    <span
-                      style={{ textDecoration: "underline", color: "teal" }}
-                    >
-                      {creator}
-                    </span>
-                  </Link>
-                </Text>
-                <Text fontSize="sm" color="gray.500">
-                  <a href={modelUrl}>
-                    <span
-                      style={{ textDecoration: "underline", color: "teal" }}
-                    >
-                      Try on Replicate <ExternalLinkIcon />{" "}
-                    </span>
-                  </a>
-                </Text>
-              </Box>
-            )
-          )}
+          .map((model) => (
+            <ModelCard key={model.id} model={model} />
+          ))}
       </SimpleGrid>
     </Box>
   );
