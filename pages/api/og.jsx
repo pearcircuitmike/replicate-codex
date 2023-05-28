057464f9c870efce8b52e171ad3ed8326f467414
Michael Young <msyoung2012@gmail.com>
2023-05-28 17:00:39 -0400

    undo

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -41,9 +41,9 @@ export default async function handler(request) {
   const avgCompletionTime = searchParams.get("avgCompletionTime");
   const predictionHardware = searchParams.get("predictionHardware");
 
-  const maxLineLength = 69; // Set the maximum length to fit your design.
+  let maxLineLength = 69; // Set the maximum length to fit your design.
 
-  const truncatedDescription = ogModelDescription;
+  let truncatedDescription = ogModelDescription;
 
   if (ogModelDescription.length > maxLineLength) {
     truncatedDescription =
