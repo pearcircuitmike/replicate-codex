ad69485ecf3fe58dae63df2c92a217530744b6aa
Michael Young <msyoung2012@gmail.com>
2023-06-08 16:27:12 -0400

    Redirect for broken links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/models/:id(\\d{3})",
+        destination: "/models/replicate/:id",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
