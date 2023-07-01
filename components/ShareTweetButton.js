09f3e90ba3a2599dc2d05dfff007191d5991247f
Michael Young <msyoung2012@gmail.com>
2023-07-01 13:35:04 -0400

    Fix twitter share feature
    Have the correct model name

--- components/ShareTweetButton.js
+++ components/ShareTweetButton.js
@@ -3,7 +3,7 @@ import { Button } from "@chakra-ui/react";
 export default function ShareTweetButton() {
   function shareOnTwitter() {
     const tweetText = encodeURIComponent(
-      "Check out this ReplicateCodex page: "
+      "Check out this model I found on AIModels.fyi: "
     );
     const tweetUrl = encodeURIComponent(window.location.href);
     const shareUrl = `https://twitter.com/intent/tweet?text=${tweetText}&url=${tweetUrl}`;
