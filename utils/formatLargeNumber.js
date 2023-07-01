8de6809f2e13470aa4fd7adbbc70f78e0eed6c62
Michael Young <msyoung2012@gmail.com>
2023-07-01 15:41:22 -0400

    Fix issue with nulls

--- utils/formatLargeNumber.js
+++ utils/formatLargeNumber.js
@@ -1,5 +1,7 @@
 export const formatLargeNumber = (number) => {
-  if (number >= 1000000) {
+  if (number === null || number === undefined) {
+    return "-";
+  } else if (number >= 1000000) {
     return (number / 1000000).toFixed(1) + "M";
   } else if (number >= 1000) {
     return (number / 1000).toFixed(1) + "K";
