80423d18fc48ed0ef15ecf80476a6ab4004e2f80
Michael Young <msyoung2012@gmail.com>
2023-07-02 21:52:43 -0400

    Try to fix 404s

--- pages/models/[platform]/[model].js
+++ pages/models/[platform]/[model].js
@@ -42,7 +42,7 @@ export async function getStaticPaths() {
     }
   }
 
-  return { paths, fallback: false };
+  return { paths, fallback: "blocking" };
 }
 
 export async function getStaticProps({ params }) {
