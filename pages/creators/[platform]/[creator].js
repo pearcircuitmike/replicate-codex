14dd7043d0691f3460c91cd8e7ad29e876302a68
Michael Young <msyoung2012@gmail.com>
2023-07-19 18:47:05 -0400

    add console log

--- pages/creators/[platform]/[creator].js
+++ pages/creators/[platform]/[creator].js
@@ -42,6 +42,7 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Creator({ creator, models, allModels, platform }) {
+  console.log(allModels);
   const avgCost =
     models
       .filter((model) => model.costToRun !== "")
