105533fea645688f7015324fc2865c62cfe8a3d6
Michael Young <msyoung2012@gmail.com>
2023-07-24 16:28:35 -0400

    handle null case and capitalize AI
    Nicer style

--- utils/kebabToTitleCase.js
+++ utils/kebabToTitleCase.js
@@ -1,6 +1,12 @@
 export const kebabToTitleCase = (str) => {
+  if (str === null || str === undefined) {
+    return "";
+  }
+
   return str
     .split("-")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) =>
+      word === "ai" ? "AI" : word.charAt(0).toUpperCase() + word.slice(1)
+    )
     .join(" ");
 };
