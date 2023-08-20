3971a61312894bb9fb1b2b523540b4e9038f86ff
Michael Young <msyoung2012@gmail.com>
2023-08-20 10:14:54 -0400

    Add quick search

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,159 +2,132 @@ import Link from "next/link";
 import {
   Heading,
   HStack,
+  VStack,
   Flex,
   IconButton,
-  Spacer,
-  Image,
-  VStack,
+  Box,
+  Input,
+  InputGroup,
+  InputLeftElement,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { useState } from "react";
+import { useRouter } from "next/router";
+import { SearchIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
   const [display, changeDisplay] = useState("none");
+  const router = useRouter();
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    const query = e.target.query.value;
+    router.push(`/results?query=${encodeURIComponent(query)}`);
+  };
 
   return (
     <nav>
-      <Flex p={5}>
-        <Flex top="1rem" right="1rem" align="center">
-          {/*LOGO*/}
+      <Flex p={5} justify="space-between" align="center">
+        <Box>
           <Heading fontSize="2xl">
-            <Link href="/" aria-label="Home" my={5} w="100%">
+            <Link href="/" aria-label="Home">
               <div>🤖 AIModels.fyi</div>
             </Link>
           </Heading>
-        </Flex>
-        <Spacer />
-        <Flex top="1rem" right="1rem" align="center">
-          {/* DESKTOP */}
+        </Box>
 
-          <Flex display={["none", "none", "flex", "flex"]}>
-            <HStack spacing={10}>
-              <Link
-                href="https://www.passionfroot.me/replicate-codex"
-                aria-label="advertise"
-                m={5}
-                w="100%"
-              >
-                Advertise
-              </Link>
-              <Link href="/creators" aria-label="Creators" m={5} w="100%">
-                Creators
-              </Link>
-              <Link href="/models" aria-label="Models" m={5} w="100%">
-                Models
-              </Link>
-              <Link href="/trending" aria-label="trending" m={5} w="100%">
-                🔥 Trending
-              </Link>
-              <Link
-                href="https://aimodels.discourse.group/"
-                aria-label="forum"
-                m={5}
-                w="100%"
-              >
-                Forum
-              </Link>
+        <Box flex="1" maxW="600px" mx="auto">
+          <form onSubmit={handleSearchSubmit}>
+            <InputGroup size="md">
+              <InputLeftElement
+                pointerEvents="none"
+                children={<SearchIcon color="gray.300" />}
+              />
+              <Input type="text" name="query" placeholder="Quick Search..." />
+            </InputGroup>
+          </form>
+        </Box>
 
-              <Link
-                href="https://notes.aimodels.fyi"
-                aria-label="notes"
-                m={5}
-                w="100%"
-              >
-                Notes
-              </Link>
-            </HStack>
-          </Flex>
-
-          {/* MOBILE */}
+        <HStack spacing={10} display={["none", "none", "flex", "flex"]}>
+          <Link
+            href="https://www.passionfroot.me/replicate-codex"
+            aria-label="advertise"
+          >
+            Advertise
+          </Link>
+          <Link href="/creators" aria-label="Creators">
+            Creators
+          </Link>
+          <Link href="/models" aria-label="Models">
+            Models
+          </Link>
+          <Link href="/trending" aria-label="trending">
+            🔥 Trending
+          </Link>
+          <Link href="https://notes.aimodels.fyi" aria-label="guides">
+            Guides
+          </Link>
           <IconButton
             aria-label="Open Menu"
             size="lg"
-            mr={2}
             icon={<HamburgerIcon />}
             onClick={() => changeDisplay("flex")}
             display={["flex", "flex", "none", "none"]}
-          ></IconButton>
-        </Flex>
+          />
+        </HStack>
+      </Flex>
 
-        {/* MOBILE CONTENT */}
-
-        <Flex
-          w="100vw"
-          display={display}
-          bgColor="gray.50"
-          zIndex={20}
-          h="100vh"
-          pos="fixed"
-          top="0"
-          left="0"
-          overflowY="auto"
-          flexDir="column"
-        >
-          <HStack m={5}>
-            <Flex top="1rem" align="center">
-              {/*LOGO*/}
-              <Heading fontSize="2xl">
-                <Link href="/" aria-label="Home" my={5} w="100%">
-                  <span onClick={() => changeDisplay("none")}>
-                    🤖 AIModels.fyi
-                  </span>
-                </Link>
-              </Heading>
-            </Flex>
-            <Spacer />
-            <Flex>
-              <IconButton
-                mr={2}
-                aria-label="Close Menu"
-                size="lg"
-                icon={<CloseIcon />}
-                onClick={() => changeDisplay("none")}
-              ></IconButton>
-            </Flex>
-          </HStack>
-
-          <Flex flexDir="column" align="center">
-            <VStack spacing={3} m={5} fontSize="xl">
-              <Link href="/creators" aria-label="creators" m={"10px"} w="100%">
-                <span onClick={() => changeDisplay("none")}>Creators</span>
+      <Flex
+        w="100vw"
+        display={display}
+        bgColor="gray.50"
+        zIndex={20}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+      >
+        <HStack m={5}>
+          <Flex top="1rem" align="center">
+            <Heading fontSize="2xl">
+              <Link href="/" aria-label="Home">
+                <span onClick={() => changeDisplay("none")}>
+                  🤖 AIModels.fyi
+                </span>
               </Link>
-              <Link href="/models" aria-label="models" m={"10px"} w="100%">
-                <span onClick={() => changeDisplay("none")}>Models </span>
-              </Link>
-              <Link href="/trending" aria-label="trending" m={"10px"} w="100%">
-                <span onClick={() => changeDisplay("none")}>Trending </span>
-              </Link>
-
-              <Link
-                href="https://notes.aimodels.fyi"
-                aria-label="notes"
-                m={"10px"}
-                w="100%"
-              >
-                <span onClick={() => changeDisplay("none")}>Notes</span>
-              </Link>
-              <Link
-                href="https://aimodels.discourse.group/"
-                aria-label="forum"
-                m={"10px"}
-                w="100%"
-              >
-                <span onClick={() => changeDisplay("none")}>Forum </span>
-              </Link>
-
-              <Link
-                href="https://www.passionfroot.me/replicate-codex"
-                aria-label="advertise"
-                m={"10px"}
-                w="100%"
-              >
-                <span onClick={() => changeDisplay("none")}>Advertise</span>
-              </Link>
-            </VStack>
+            </Heading>
           </Flex>
+          <IconButton
+            mr={2}
+            aria-label="Close Menu"
+            size="lg"
+            icon={<CloseIcon />}
+            onClick={() => changeDisplay("none")}
+          />
+        </HStack>
+        <Flex flexDir="column" align="center">
+          <VStack spacing={3} m={5} fontSize="xl">
+            <Link href="/creators" aria-label="creators">
+              <span onClick={() => changeDisplay("none")}>Creators</span>
+            </Link>
+            <Link href="/models" aria-label="models">
+              <span onClick={() => changeDisplay("none")}>Models</span>
+            </Link>
+            <Link href="/trending" aria-label="trending">
+              <span onClick={() => changeDisplay("none")}>Trending</span>
+            </Link>
+            <Link href="https://notes.aimodels.fyi" aria-label="guides">
+              <span onClick={() => changeDisplay("none")}>Guides</span>
+            </Link>
+            <Link
+              href="https://www.passionfroot.me/replicate-codex"
+              aria-label="advertise"
+            >
+              <span onClick={() => changeDisplay("none")}>Advertise</span>
+            </Link>
+          </VStack>
         </Flex>
       </Flex>
     </nav>
