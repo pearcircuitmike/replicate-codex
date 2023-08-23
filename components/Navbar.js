026e75e03c0fcb22e58e9538c47728b1cbf16de7
Michael Young <msyoung2012@gmail.com>
2023-08-23 08:53:17 -0400

    Clean up navabar
    Fix search issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 import {
   Heading,
   HStack,
-  VStack,
   Flex,
   IconButton,
-  Box,
+  Spacer,
   Input,
+  VStack,
+  Box,
   InputGroup,
   InputLeftElement,
 } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon, SearchIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { SearchIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
   const [display, changeDisplay] = useState("none");
@@ -29,11 +29,13 @@ const Navbar = () => {
     <nav>
       <Flex p={5} justify="space-between" align="center">
         <Box>
-          <Heading fontSize="2xl">
-            <Link href="/" aria-label="Home">
-              <div>🤖 AIModels.fyi</div>
-            </Link>
-          </Heading>
+          <Flex top="1rem" right="1rem" align="center">
+            <Heading fontSize="2xl">
+              <Link href="/" aria-label="Home">
+                <div>🤖 AIModels.fyi</div>
+              </Link>
+            </Heading>
+          </Flex>
         </Box>
 
         <Box flex="1" maxW="600px" mx="auto">
@@ -43,90 +45,129 @@ const Navbar = () => {
                 <SearchIcon color="gray.300" />
               </InputLeftElement>
               <Input type="text" name="query" placeholder="Quick Search..." />
-            </InputGroup>
-          </form>
+        {/* Quick Search Bar for Desktop */}
+        <Box
+          flex="1"
+          maxW="600px"
+          mx="auto"
+          display={["none", "none", "flex", "flex"]}
+        >
+          <Box flex="1" maxW="600px" mx="auto" mx="20px">
+            <form onSubmit={handleSearchSubmit}>
+              <InputGroup size="md">
+                <InputLeftElement pointerEvents="none">
+                  <SearchIcon color="gray.300" />
+                </InputLeftElement>
+                <Input type="text" name="query" placeholder="Quick Search..." />
+              </InputGroup>
+            </form>
+          </Box>
         </Box>
 
-        <HStack spacing={10} display={["none", "none", "flex", "flex"]}>
-          <Link
-            href="https://www.passionfroot.me/replicate-codex"
-            aria-label="advertise"
-          >
-            Advertise
-          </Link>
-          <Link href="/creators" aria-label="Creators">
-            Creators
-          </Link>
-          <Link href="/models" aria-label="Models">
-            Models
-          </Link>
-          <Link href="/trending" aria-label="trending">
-            🔥 Trending
-          </Link>
-          <Link href="https://notes.aimodels.fyi" aria-label="guides">
-            Guides
-          </Link>
+        <Flex top="1rem" right="1rem" align="center">
+          <Flex display={["none", "none", "flex", "flex"]}>
+            <HStack spacing={10}>
+              <Link
+                href="https://www.passionfroot.me/replicate-codex"
+                aria-label="Advertise"
+                m={5}
+                w="100%"
+              >
+                Advertise
+              </Link>
+              <Link href="/creators" aria-label="Creators" m={5} w="100%">
+                Creators
+              </Link>
+              <Link href="/models" aria-label="Models" m={5} w="100%">
+                Models
+              </Link>
+              <Link href="/trending" aria-label="Trending" m={5} w="100%">
+                🔥 Trending
+              </Link>
+
+              <Link
+                href="https://notes.aimodels.fyi"
+                aria-label="Notes"
+                m={5}
+                w="100%"
+              >
+                Notes
+              </Link>
+            </HStack>
+          </Flex>
           <IconButton
             aria-label="Open Menu"
             size="lg"
+            mr={2}
             icon={<HamburgerIcon />}
             onClick={() => changeDisplay("flex")}
             display={["flex", "flex", "none", "none"]}
-          />
-        </HStack>
-      </Flex>
+          ></IconButton>
+        </Flex>
 
-      <Flex
-        w="100vw"
-        display={display}
-        bgColor="gray.50"
-        zIndex={20}
-        h="100vh"
-        pos="fixed"
-        top="0"
-        left="0"
-        overflowY="auto"
-        flexDir="column"
-      >
-        <HStack m={5}>
-          <Flex top="1rem" align="center">
-            <Heading fontSize="2xl">
-              <Link href="/" aria-label="Home">
-                <span onClick={() => changeDisplay("none")}>
-                  🤖 AIModels.fyi
-                </span>
+        <Flex
+          w="100vw"
+          display={display}
+          bgColor="gray.50"
+          zIndex={20}
+          h="100vh"
+          pos="fixed"
+          top="0"
+          left="0"
+          overflowY="auto"
+          flexDir="column"
+        >
+          <HStack m={5}>
+            <Flex top="1rem" align="center">
+              <Heading fontSize="2xl">
+                <Link href="/" aria-label="Home" my={5}>
+                  <span onClick={() => changeDisplay("none")}>
+                    🤖 AIModels.fyi
+                  </span>
+                </Link>
+              </Heading>
+            </Flex>
+            <Spacer />
+            <Flex>
+              <IconButton
+                mr={2}
+                aria-label="Close Menu"
+                size="lg"
+                icon={<CloseIcon />}
+                onClick={() => changeDisplay("none")}
+              ></IconButton>
+            </Flex>
+          </HStack>
+          <Flex flexDir="column" align="center">
+            <VStack spacing={3} m={5} fontSize="xl">
+              <Link href="/creators" aria-label="Creators" m={"10px"} w="100%">
+                <span onClick={() => changeDisplay("none")}>Creators</span>
               </Link>
-            </Heading>
+              <Link href="/models" aria-label="Models" m={"10px"} w="100%">
+                <span onClick={() => changeDisplay("none")}>Models</span>
+              </Link>
+              <Link href="/trending" aria-label="Trending" m={"10px"} w="100%">
+                <span onClick={() => changeDisplay("none")}>Trending</span>
+              </Link>
+
+              <Link
+                href="https://notes.aimodels.fyi"
+                aria-label="Notes"
+                m={"10px"}
+                w="100%"
+              >
+                <span onClick={() => changeDisplay("none")}>Notes</span>
+              </Link>
+              <Link
+                href="https://www.passionfroot.me/replicate-codex"
+                aria-label="Advertise"
+                m={"10px"}
+                w="100%"
+              >
+                <span onClick={() => changeDisplay("none")}>Advertise</span>
+              </Link>
+            </VStack>
           </Flex>
-          <IconButton
-            mr={2}
-            aria-label="Close Menu"
-            size="lg"
-            icon={<CloseIcon />}
-            onClick={() => changeDisplay("none")}
-          />
-        </HStack>
-        <Flex flexDir="column" align="center">
-          <VStack spacing={3} m={5} fontSize="xl">
-            <Link href="/creators" aria-label="creators">
-              <span onClick={() => changeDisplay("none")}>Creators</span>
-            </Link>
-            <Link href="/models" aria-label="models">
-              <span onClick={() => changeDisplay("none")}>Models</span>
-            </Link>
-            <Link href="/trending" aria-label="trending">
-              <span onClick={() => changeDisplay("none")}>Trending</span>
-            </Link>
-            <Link href="https://notes.aimodels.fyi" aria-label="guides">
-              <span onClick={() => changeDisplay("none")}>Guides</span>
-            </Link>
-            <Link
-              href="https://www.passionfroot.me/replicate-codex"
-              aria-label="advertise"
-            >
-              <span onClick={() => changeDisplay("none")}>Advertise</span>
-            </Link>
-          </VStack>
         </Flex>
       </Flex>
     </nav>
