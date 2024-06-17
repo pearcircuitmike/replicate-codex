18e8b69d69debbd9f815ef8be8e0458e8f26bfc9
Michael Young <msyoung2012@gmail.com>
2024-06-17 19:29:38 -0400

    caching attempt

--- next.config.js
+++ next.config.js
@@ -556,6 +556,20 @@ const nextConfig = {
     // return the combined list of redirects
     return [...existingRedirects, ...newRedirects, ...rearchitectureRedirects];
   },
+
+  async headers() {
+    return [
+      {
+        source: "/papers/:platform/:paper*",
+        headers: [
+          {
+            key: "Cloudflare-CDN-Cache-Control",
+            value: "public, s-maxage=86400, stale-while-revalidate=3600",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
