6c9b643e7b9cf236cf989f48fe8b1686a15d491f
Michael Young <msyoung2012@gmail.com>
2024-07-30 19:28:59 -0400

    remove runtime = edge

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -1,9 +1,5 @@
 import { ImageResponse } from "@vercel/og";
 
-export const config = {
-  runtime: "edge",
-};
-
 export default async function handler(req) {
   try {
     const { searchParams } = new URL(req.url);
