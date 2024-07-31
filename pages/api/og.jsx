f1b93f3e5e742fe4b307e4ec9fbe1d34461529d1
Michael Young <msyoung2012@gmail.com>
2024-07-30 20:03:18 -0400

    update dep

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -1,5 +1,9 @@
 import { ImageResponse } from "@vercel/og";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default async function handler(req) {
   try {
     const { searchParams } = new URL(req.url);
