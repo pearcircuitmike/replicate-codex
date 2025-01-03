51e6ad713a0544f686ae0f5e6599bc8ab4ddea14
Michael Young <msyoung2012@gmail.com>
2025-01-02 21:32:27 -0500

    Update next-sitemap config - controls robots.txt
    It's actually controlled here

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,4 @@
+// next-sitemap.js
 const siteUrl = "https://www.aimodels.fyi";
 
 module.exports = {
@@ -6,4 +7,20 @@ module.exports = {
   exclude: ["/components/*"],
   generateIndexSitemap: true,
   sitemapSize: 20000,
+  robotsTxtOptions: {
+    // This creates custom rules
+    policies: [
+      {
+        userAgent: "bingbot",
+        // Use a wildcard so it covers query strings like /api/og?title=...
+        disallow: ["/api/og*"],
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    // Optionally add your Host and any additional sitemaps
+    additionalSitemaps: ["https://www.aimodels.fyi/sitemap.xml"],
+  },
 };
