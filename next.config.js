ff5114ab70cfc0c10d724b2b34a1835ee509a943
Michael Young <msyoung2012@gmail.com>
2025-01-08 20:20:43 -0500

    Further remove redirects
    Eliminating more

--- next.config.js
+++ next.config.js
@@ -1,63 +1,29 @@
 /** @type {import('next').NextConfig} */
-const fs = require("fs");
-const path = require("path");
-
-// load the URL map from urlMap.json
-const urlMap = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "./urlMap.json"), "utf-8")
-);
-
 const nextConfig = {
   experimental: {
     ppr: true,
   },
   reactStrictMode: true,
-  // Allow Next.js to optimize images from these domains
+
   images: {
-    domains: [
-      "tjzk.replicate.delivery",
-      "pbxt.replicate.delivery",
-      "replicate.delivery",
-      "arxiv.org",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tjzk.replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "pbxt.replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+      {
+        protocol: "https",
+        hostname: "arxiv.org",
+      },
     ],
-  },
-
-  async redirects() {
-    // existing redirects
-    const existingRedirects = [
-      {
-        source: "/models/:id(\\d{3})",
-        destination: "/models/replicate/:id",
-        permanent: true,
-      },
-      {
-        source: "/models/:id(\\d{2})",
-        destination: "/models/replicate/:id",
-        permanent: true,
-      },
-      {
-        source: "/models/:id(\\d{1})",
-        destination: "/models/replicate/:id",
-        permanent: true,
-      },
-      {
-        source: "/creators/:id",
-        destination: "/creators/replicate/:id",
-        permanent: true,
-      },
-    ];
-
-    // new redirects based on urlMap
-    const newRedirects = urlMap.map((mapping) => ({
-      source: `/models/replicate/${mapping.old_id}`,
-      destination: `/models/replicate/${mapping.id}`,
-      permanent: true,
-    }));
-
-    // We removed the rearchitectureRedirects here
-
-    // return the combined list of redirects
-    return [...existingRedirects, ...newRedirects];
   },
 };
 
